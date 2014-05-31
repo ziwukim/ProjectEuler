736399c20a40781bc8f5c0dc034aad0293881640
Jiwoo Kim <ziwukim@gmail.com>
2014-05-31 01:48:35 -0700

    update

--- p4.py
+++ p4.py
@@ -1,11 +1,10 @@
 from palchecker import *
 
 max = 0
-for i in range(100):
-	for j in range(100):
+for i in range(1000):	# This is not two-digit's multiplication
+	for j in range(1000):
 		prod = i*j
 		if palchecker(prod):
 			if prod > max:
 				max = prod
-				print prod 
 print (max)
